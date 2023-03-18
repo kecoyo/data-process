18fc4aa788b237ffe7892d3434c9f98e823ce5dd
杨可可 <kecoyo@163.com>
2023-03-18 23:41:33 +0800

    add baidu-fanyi

---
+++ apps/baidu-fanyi/txt2csv.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const fsExtra = require('../../common/fs-extra');
+
+const content = fsExtra.readFileSync(path.join(__dirname, './txt2csv.txt'), 'utf-8');
+
+let lines = content.split('\n').filter(line => line.trim());
+
+let rows = [];
+for (let i = 0; i < lines.length / 2; i++) {
+  let row = {
+    zh_name: lines[i * 2],
+    en_name: lines[i * 2 + 1],
+  };
+  console.log('🚀 ~ row:', row);
+  rows.push(row);
+}
+
+fsExtra.writeCsv(path.join(__dirname, './add_collection.csv'), rows, {
+  headers: true,
+});

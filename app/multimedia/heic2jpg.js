30aaec32d9bfa88877e01f5cfadd59a6f6e9c642
杨可可 <kecoyo@163.com>
2023-06-07 22:29:50 +0800

    批量heic图片转jpg

--- app/multimedia/heic2jpg.js
+++ app/multimedia/heic2jpg.js
@@ -1,29 +1,39 @@
 const path = require('path');
+const util = require('util');
 const Task = require('../common/task');
 const fs = require('../common/fs-extra');
 const { spawn } = require('../common/child_process');
 
-const rootPath = 'D:\\heic';
+const options = {
+  rootDir: { type: 'string', default: 'D:\\heic' },
+  fileFilter: { type: 'string', default: '*.heic' },
+  outExtName: { type: 'string', default: '.jpg' },
+  outQuality: { type: 'string', default: '90%' },
+  outSuffix: { type: 'string', default: '' },
+};
+const args = util.parseArgs({ options }).values;
+console.log('args:', JSON.stringify(args));
 
 /**
  * 批量heic图片转jpg
  */
 Task.createTask({
   input: async () => {
-    const list = await fs.readdirp(rootPath, { fileFilter: '*.heic' });
-    return list;
+    const list = await fs.readdirp(args.rootDir, { fileFilter: args.fileFilter });
+    return list.map(entry => ({ fromFile: entry.fullPath }));
   },
-  concurrency: 1,
+  // concurrency: 1,
   processRow: async (row, i) => {
-    const heicFile = row.fullPath;
-    const heicFileName = row.basename;
+    const fromFile = row.fromFile;
+    const fromFileName = path.basename(fromFile);
+    const curDir = path.dirname(fromFile);
+    const fileName = fromFileName.replace(path.extname(fromFile), '');
 
-    const currPath = path.dirname(heicFile);
-    const fileName = heicFileName.substring(0, heicFileName.lastIndexOf('.'));
+    const toFileName = fileName + args.outSuffix + args.outExtName;
+    const toFile = path.join(curDir, toFileName);
 
-    const jpgFileName = fileName + '.jpg';
-    const jpgFile = path.join(currPath, jpgFileName);
+    await spawn('magick', [fromFile, '-quality', args.outQuality, toFile]);
 
-    await spawn('magick', [row.fullPath, '-quality', '90%', jpgFile]);
+    row.toFile = toFile;
   },
 });
